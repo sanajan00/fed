fe6f175d18adf1843ebb01e1284c8805ac31b799
Arpine <arpine.sanajan@hva.nl>
2025-11-21 14:27:56 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+const menuButton = document.querySelector("nav button");
+const deNav = document.querySelector("header nav");
+
+
+menuButton.onclick = toggleMenu;
+
+function toggleMenu() {
+    deNav.classList.toggle("is-open");
+}
